224903c5273ba0390a892740f8ea31d74f2a44dc
Preetam Adhikari <adhikaripreetam68@gmail.com>
2024-04-17 01:27:34 +0530

    my class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    //Ram Is A Good Boy . And He Knows to Everything
+        // sum() method
+        //minus() method
+    // * : Multiplication
+    // / : Division
+    // % : Modulo
+    // + : Addition
+    // – : Subtraction
+
+    }
+}
