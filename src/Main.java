603684909aff6df4ee8361080cfab0c35ac1b5a2
Preetam Adhikari <adhikaripreetam68@gmail.com>
2024-04-17 18:17:59 +0530

    changes

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,41 @@ public class Main {
     // + : Addition
     // – : Subtraction
 
+//        String aName = "Ram";
+//        String bName = "is";
+//        String cName = "a";
+//        String dName = "good";
+//        String eName = "boy.";
+//        String fName = "and";
+//        String gName = "he";
+//        String hName = "knows";
+//        String iName = "to";
+//        String jName = "everything";
+//
+//        System.out.println(aName+" "+bName+" "+cName+" "+dName+" "+eName+" "+fName+" "+gName+" "+hName+" "+iName+" "+jName);
+//
+//        int a = 20;
+//        int b = 30;
+//        System.out.println("The Sum Of a and b Is = " + Integer.sum(a,b) );
+//
+//        int c = 818;
+//        int d = 474;
+//        System.out.println("The Sum of c and d is = " + Integer.sum(c,d));
+//
+//        long x = 100;
+//        long y = 20;
+//        System.out.println("The Subtraction is = " + Math.subtractExact(x,y
+//        ));
+//
+//
+//        long p = 574;
+//        long m = 254;
+//        System.out.println("The Subtraction is = " + Math.subtractExact(p,m));
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a/b);
+
     }
+
 }
