701d0802fbf92d02e587a33c9d882f97403f348e
Preetam Adhikari <adhikaripreetam68@gmail.com>
2024-04-20 21:46:03 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-
-import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+//        int a = 6*5-34/2;
+//        System.out.println(a);
+//
+//        int b = 34/2-6*5+15;
+//        System.out.println(b);
 
-//        String[] name = {"Preetam","Arijit"};
-//        System.out.println(Arrays.toString(name));
+        /*int c = 4/2-2*2;
+        System.out.println(c);*/
 
-
+        int a = 10;
+//        int b = a++;
+//        int c = ++a;
+        System.out.println(++a);
 
     }
-
 }
